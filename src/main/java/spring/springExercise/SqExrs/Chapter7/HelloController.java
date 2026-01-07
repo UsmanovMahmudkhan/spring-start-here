d08c0c3a62518836c8492d49685e7527b4a355ab
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-01-07 22:35:08 +0900

    used @Controller and @RequestMapping annotation

---
+++ src/main/java/spring/springExercise/SqExrs/Chapter7/HelloController.java
@@ -0,0 +1,16 @@
+package spring.springExercise.SqExrs.Chapter7;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/home")
+    public String getHome(){
+        return "home.html";
+    }
+}
+
---
+++ src/test/java/spring/springExercise/SqExrs/Chapter7/springBoot/src/test/java/chapter7/ApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.springExercise.SqExrs.chapter7.springBoot.src.test.java.chapter7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
