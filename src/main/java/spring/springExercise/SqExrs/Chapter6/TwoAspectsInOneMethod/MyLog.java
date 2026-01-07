52165271ea40053b37210c186bb62a89fec547e9
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-01-07 18:55:20 +0900

    used custom annotation to mark method as intercepted method to work with aspect

---
+++ src/main/java/spring/springExercise/SqExrs/Chapter6/TwoAspectsInOneMethod/MyLog.java
@@ -0,0 +1,11 @@
+package spring.springExercise.SqExrs.Chapter6.TwoAspectsInOneMethod;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface MyLog {
+}
