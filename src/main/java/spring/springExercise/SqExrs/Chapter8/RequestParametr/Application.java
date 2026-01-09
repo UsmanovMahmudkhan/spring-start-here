9d7d2e75107f350c53061aaa8dc8d73e6324e998
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-01-09 17:51:53 +0900

    recieved clients Https request and took info, and send to Model view to render and show in Html format using @RequestParam

---
+++ src/main/java/spring/springExercise/SqExrs/Chapter8/RequestParametr/Application.java
@@ -0,0 +1,11 @@
+package spring.springExercise.SqExrs.Chapter8.RequestParametr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "spring.springExercise.SqExrs.Chapter8.RequestParametr")
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
