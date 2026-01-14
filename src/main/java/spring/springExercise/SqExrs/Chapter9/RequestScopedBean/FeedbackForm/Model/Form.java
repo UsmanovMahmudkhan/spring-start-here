7755e6141e5915d729d5a330112336149eb6c098
Usmonov Mahmudkhon <usmanovmahmudkhan@gmail.com>
2026-01-14 17:10:32 +0900

    creating form, using @RequestScope bean

---
+++ src/main/java/spring/springExercise/SqExrs/Chapter9/RequestScopedBean/FeedbackForm/Model/Form.java
@@ -0,0 +1,44 @@
+package spring.springExercise.SqExrs.Chapter9.RequestScopedBean.FeedbackForm.Model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Form {
+    String name;
+    String email;
+    String text;
+
+    public Form() {
+    }
+
+    public Form(String name, String email, String text) {
+        this.name = name;
+        this.email = email;
+        this.text = text;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+}
---
+++ src/main/java/spring/springExercise/SqExrs/Chapter9/RequestScopedBean/FeedbackForm/Service/FormService.java
@@ -0,0 +1,40 @@
+package spring.springExercise.SqExrs.Chapter9.RequestScopedBean.FeedbackForm.Service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.RequestScope;
+import spring.springExercise.SqExrs.Chapter9.RequestScopedBean.FeedbackForm.Model.Form;
+
+
+@Service
+@RequestScope
+public class FormService {
+
+    private final Form form;
+
+    public FormService(Form form) {
+        this.form = form;
+    }
+
+    public Form process(){
+        String name = form.getName();
+        String email = form.getEmail();
+        String text = form.getText();
+
+        boolean hasEmptyField =
+                name == null || name.isBlank() ||
+                email == null || email.isBlank() ||
+                text == null || text.isBlank();
+
+        if (hasEmptyField) {
+            form.setName("Failed to submit");
+            form.setEmail(null);
+            form.setText(null);
+        }
+
+        return form;
+    }
+
+    public Form getForm() {
+        return form;
+    }
+}
